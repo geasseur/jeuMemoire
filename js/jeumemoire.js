61ad3bf7bce7e0c1b83bfbfed5c8a27606ae7fc4
geasseur <baptiste.ventura@neuf.fr>
2017-07-12 15:45:59 +0200

    script for click cards in progress

--- js/jeumemoire.js
+++ js/jeumemoire.js
@@ -1,7 +1,12 @@
+
+//variable creationJeu
 var couleur = ["red","red","blue","blue","green","green","yellow","yellow","black","black","purple","purple","brown","brown"];
-var random = 0;
 var randomCouleur = ["vide","vide","vide","vide","vide","vide","vide","vide","vide","vide","vide","vide","vide","vide"];
+var random = 0;
 var listeCarte;
+
+//variable lancementJeu
+
 
 function creationJeu(){
   var pseudo = prompt("entrez votre pseudo");
@@ -10,6 +15,7 @@ function creationJeu(){
     random = Math.floor((Math.random() * couleur.length));
     if(randomCouleur[random] == "vide"){
     randomCouleur[random] = couleur[i];
+
     }
     else{
       i--;
@@ -19,11 +25,16 @@ function creationJeu(){
   listeCarte = document.getElementsByClassName("carte");
   console.log(listeCarte);
   for(i = 0; i < randomCouleur.length; i++){
-    listeCarte[i].style.background-color = randomCouleur[i];
-
+    //listeCarte[i].style.backgroundcolor = randomCouleur[i];
+    listeCarte[i] = randomCouleur[i];
   }
   console.log(randomCouleur);
-
+  listeCarte[i].onclick = lancementJeu;
 }
 
-function
+function lancementJeu(){
+  for(i = 0; i < listeCarte.length; i++){
+    listeCarte[i].style.backgroundcolor = listeCarte[i];
+  }
+
+}
