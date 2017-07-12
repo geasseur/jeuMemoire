b735c2cc21664627a4c87591f75f89c4a43fcd1b
geasseur <baptiste.ventura@neuf.fr>
2017-07-12 18:35:42 +0200

    selection carte fait, récupération des indices des deux cartes choisie fait

--- js/jeumemoire.js
+++ js/jeumemoire.js
@@ -1,3 +1,5 @@
+var pointJoueur = 0;
+
 
 //variable creationJeu
 var couleur = ["red","red","blue","blue","green","green","yellow","yellow","black","black","purple","purple","brown","brown"];
@@ -6,35 +8,49 @@ var random = 0;
 var listeCarte;
 
 //variable lancementJeu
-
+var nbCarte;
+var nbEssais = 0;
+var test1 = 0;
+var test2 = 0;
 
 function creationJeu(){
-  var pseudo = prompt("entrez votre pseudo");
-  document.getElementById("pseudoJoueur").innerHTML = pseudo;
+  //var pseudo = prompt("entrez votre pseudo");
+  //document.getElementById("pseudoJoueur").innerHTML = pseudo;
   for(var i = 0; i<couleur.length; i++){
     random = Math.floor((Math.random() * couleur.length));
     if(randomCouleur[random] == "vide"){
     randomCouleur[random] = couleur[i];
-
     }
     else{
       i--;
     }
+  }
 
-  }
-  listeCarte = document.getElementsByClassName("carte");
-  console.log(listeCarte);
-  for(i = 0; i < randomCouleur.length; i++){
-    //listeCarte[i].style.backgroundcolor = randomCouleur[i];
-    listeCarte[i] = randomCouleur[i];
-  }
   console.log(randomCouleur);
-  listeCarte[i].onclick = lancementJeu;
+
+  console.log(listeCarte);
 }
 
-function lancementJeu(){
-  for(i = 0; i < listeCarte.length; i++){
-    listeCarte[i].style.backgroundcolor = listeCarte[i];
-  }
+function essais(nbCarte){
+  listeCarte = document.getElementsByClassName("carte");
+    listeCarte[nbCarte].style.backgroundColor = randomCouleur[nbCarte];
+    nbEssais++;
+    console.log(nbEssais);
 
+    if(nbEssais == 1){
+      test1 = nbCarte;
+    }
+    else if (nbEssais ==2) {
+      test2 = nbCarte;
+    }
+    console.log(test1,test2);
+    if(test1 =! 0 && test2 != 0){
+      if(listeCarte[test1].outlineStyle.backgroundColor == listeCarte[test1].outlineStyle.backgroundColor){
+        pointJoueur += 1
+      }
+      else{
+        listeCarte[test1].style.backgroundColor = "white";
+        listeCarte[test2].style.backgroundColor = "white";
+      }
+    }
 }
